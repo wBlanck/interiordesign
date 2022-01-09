e0edce72c58e89532e9b3e443a90a7a1bad568d5
wblanck <blanck.william@gmail.com>
2022-01-09 14:14:54 +0100

    header fixed pos

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,19 @@ module.exports = {
       "primary-heading": "#244D4D",
       "secondary-heading": "#3D3D3F",
       "primary-text": "#5F5F64",
+      white: "#fff",
+      "hero-bg": "rgba(255, 255, 255, 0.4)",
     },
     fontFamily: {
       noto: ["'Noto Serif JP'", "sans-serif"],
       montserrat: ["'Montserrat'", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-img":
+          "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)),url(/assets/hero-bg.jpg)",
+      },
+    },
   },
   plugins: [],
 };
